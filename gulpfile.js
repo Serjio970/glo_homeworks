0f9555ca9351f71036b09e3c05c86451475e3438
SVB <MacBook@MacBook-Air-Butovs.local>
2020-03-04 21:17:51 +0300

    feat: add header index.html

--- gulpfile.js
+++ gulpfile.js
@@ -15,3 +15,24 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+var gulp = require('gulp');
+var minify = require('gulp-minify');
+
+gulp.task('min-js', function() {
+    return gulp.src('lib/*.js')
+        .pipe(minify({
+            ext: {
+                min: '.min.js'
+            },
+            ignoreFiles: ['-min.js']
+        }))
+        .pipe(gulp.dest('lib'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('lib/*.js', ['min-js']); 
+  // Other watchers
+});
+
+gulp.task('default', ['min-js', 'watch']);
