ef29a718ae0fd2547484d8d631bcf5570fcdc911
SVB <MacBook@MacBook-Air-Butovs.local>
2020-03-16 22:38:47 +0300

    add sliders, connect swiper js, new file _projects.sass

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  var modal = $('modal'),
+$(document).ready(function () {
+  var modal = $('.modal'),
       modalBtn = $('[data-toggle="modal"]'),
       closeBtn = $('.modal__close');
 
@@ -10,4 +10,24 @@ $(document).ready(function() {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true, 
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-next');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
+
 });
